79cc19a00484572361e2fbc354a66e66c73e62d9
Cynthia Alarcon <cyn.ac.sp@gmail.com>
2017-04-13 17:17:50 +0100

    changed font size

--- createSucsessful.php
+++ createSucsessful.php
@@ -14,19 +14,12 @@
     <div class="container">
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
         <a href="logIn.php" target="_self"><img src="images/ULlogo-azul.png" alt="UL Review logo" style="width: 182px; height: 50px;"></a>
-      </div> 
+      </div>
     </div><!-- /.navbar-collapse **need to look at closing div tags-->
-      </div><!-- /.container -->
   </nav>
 
-    
+
      <!-- Welcome panel -->
     <div class="container">
        <br>
@@ -35,14 +28,14 @@
            <div class="panel-body">
              <div class="index col-xs-12 col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3 col-xl-6 col-xl-offset-3">
                 <br>
-                <h1>You have created you account!</h1>
-                <h2>Please <a href="logIn.php">login</a> to access the UL Review website.</h2>
+                <h1>You have created your account!</h1><br>
+                <h4>Please <a href="logIn.php">login</a> to access the UL Review website.</h4>
                 <br>
              </div>
            </div>
         </div>
     </div>
-             
+
 <script src="js.functions.js"></script>
 <script src="js/jquery.js"></script>
 <script src="js/bootstrap.min.js"></script>
