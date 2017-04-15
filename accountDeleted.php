b4862edc52b4f5d8aece6c0ae12f653d8bf6d6e4
Kevin Murphy <kevinmurphy@Kevins-MacBook-Pro.local>
2017-04-15 11:52:44 +0100

    added php functionality to account deletion confirmation page

--- accountDeleted.php
+++ accountDeleted.php
@@ -10,6 +10,20 @@
   <title>UL Review</title>
 </head>
 <body id="body">
+     <?php
+       session_start();
+       if (!isset($_SESSION['username'])) {
+         header("Location:/logIn.php");
+       }else{
+         /*http://php.net/manual/en/function.session-unset.php*/
+         session_start();
+         session_unset();
+         session_destroy();
+         session_write_close();
+         setcookie(session_name(),'',0,'/');
+         session_regenerate_id(true);	
+       }
+  ?>
     <nav class="navbar navbar-default">
     <div class="container">
       <!-- Brand and toggle get grouped for better mobile display -->
